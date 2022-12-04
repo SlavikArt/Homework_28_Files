8fd98490b2f916838d24af7eb4dc5cc47a71eeb8
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2022-12-04 22:10:38 +0200

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Homework_28_Files
+Д/з: Files
+
+<a href="https://gist.github.com/SlavikArt/3f425304efc92b699c6cdd2894e203c1">Gist - страница со всеми кодами проектов</a>
+
+* [Task_1](Task_1)
+
+<p align="center">
+    <h2>Task 1</h2>
+    <p></p>
+    <img src="images/Task_1.png">
+</p>
---
+++ Task_1/Task_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    SetConsoleCP(1251); 
+    SetConsoleOutputCP(1251);
+
+    string word;
+
+    FILE* f;
+
+    fopen_s(&f, "db.txt", "r");
+
+    char ryphm[30];
+
+    cout << "Введите слово: ";
+    getline(cin, word);
+
+    while (true)
+    {
+        fgets(ryphm, 199, f);
+
+        string tmp = ryphm;
+
+        if (feof(f) != 0)
+            break;
+
+        if (word[word.length() - 2] == tmp[tmp.length() - 3] && word[word.length() - 1] == tmp[tmp.length() - 2])
+        {
+            cout << tmp;
+        }
+    }
+}
